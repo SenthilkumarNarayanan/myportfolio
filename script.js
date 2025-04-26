b32c8e73bcfed0859669b3e1f057da0613c90aef
SenthilkumarNarayanan <124486793+SenthilkumarNarayanan@users.noreply.github.com>
2025-04-26 12:04:51 +0530

    add theme feature

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+const toggleButton = document.getElementById('theme-toggle');
+const currentTheme = localStorage.getItem('theme') || 'light';
+document.body.classList.add(currentTheme);
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+  document.body.classList.toggle('light');
+
+  const newTheme = document.body.classList.contains('dark') ? 'dark' : 'light';
+  localStorage.setItem('theme', newTheme);
+});
